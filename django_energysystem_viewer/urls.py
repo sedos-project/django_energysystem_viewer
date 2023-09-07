686ebc28c794f2f4bce0026e92737cad6d45bbe3
Hendrik Huyskens <hendrik.huyskens@rl-institut.de>
2023-09-07 12:22:09 +0200

    Enable sector selection for network graph

--- django_energysystem_viewer/urls.py
+++ django_energysystem_viewer/urls.py
@@ -1,12 +1,11 @@
 from django.urls import path
 
 from django_energysystem_viewer import views
-from django_energysystem_viewer.api import api
 
 app_name = "django_energysystem_viewer"
 
 
 urlpatterns = [
-    path("energysystem/", api.urls),
     path("energysystem/network/", views.NetworkView.as_view()),
+    path("energysystem/network_graph/", views.network_graph),
 ]
--- django_energysystem_viewer/views.py
+++ django_energysystem_viewer/views.py
@@ -1,5 +1,14 @@
+from django.http import HttpResponse
 from django.views.generic import TemplateView
+
+from django_energysystem_viewer import network_graph as ng
 
 
 class NetworkView(TemplateView):
     template_name = "django_energysystem_viewer/network.html"
+
+
+def network_graph(request):
+    sectors = request.GET.getlist("sectors")
+    mapping = request.GET["mapping"]
+    return HttpResponse(ng.generate_Graph(sectors, mapping).to_html())
