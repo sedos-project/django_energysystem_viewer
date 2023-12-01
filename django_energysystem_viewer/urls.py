9bb54c95bece9d5e58b0f863915dbc069c8fca4d
Hendrik Huyskens <hendrik.huyskens@rl-institut.de>
2023-12-01 10:56:30 +0100

    Add view for collection artifacts

--- django_energysystem_viewer/urls.py
+++ django_energysystem_viewer/urls.py
@@ -8,4 +8,5 @@ app_name = "django_energysystem_viewer"
 urlpatterns = [
     path("energysystem/network/", views.NetworkView.as_view()),
     path("energysystem/network_graph/", views.network_graph),
+    path("collection/<str:collection_name>/artifacts/", views.ArtifactsView.as_view()),
 ]
--- django_energysystem_viewer/views.py
+++ django_energysystem_viewer/views.py
@@ -1,3 +1,4 @@
+from data_adapter import collection
 from django.http import HttpResponse
 from django.views.generic import TemplateView
 
@@ -12,3 +13,12 @@ def network_graph(request):
     sectors = request.GET.getlist("sectors")
     mapping = request.GET["mapping"]
     return HttpResponse(ng.generate_Graph(sectors, mapping).to_html())
+
+
+class ArtifactsView(TemplateView):
+    template_name = "django_energysystem_viewer/artifacts.html"
+
+    def get_context_data(self, **kwargs):
+        collection_name = kwargs["collection_name"]
+        artifacts = collection.get_artifacts_from_collection(collection_name)
+        return {"collection_name": collection_name, "artifacts": artifacts}
